980cb61dc5e0f1c8619b26151c89b354a9deebe9
Sneha Biswas <sneha9963.0@gmail.com>
2026-06-29 23:09:45 +0530

    Add Task 01: Band Name Generator

---
+++ Task-01-Band-Name-Generator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city=input("What City did you grew up in?\n")
+pet=input("What's your pet name?\n")
+band_name=city+" "+pet
+print("Your band name could be"+" "+band_name)
